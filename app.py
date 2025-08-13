b8ce0bdb04201408f73384aab494dae6c50a224e
Edima <edimaudo@gmail.com>
2025-08-13 16:57:56 +0000

    removed emojis

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 pg = st.navigation([
-    st.Page("About.py", icon="💬"),
-    st.Page("Planet_Heater.py", icon="🌍"),
-    st.Page("Carbon_Chronicles.py", icon="🔥"),
-    st.Page("Greener_Faster.py", icon="🔍"),
+    st.Page("About.py"),
+    st.Page("Planet_Heater.py"),
+    st.Page("Carbon_Chronicles.py"),
+    st.Page("Greener_Faster.py"),
 ])
 pg.run()
