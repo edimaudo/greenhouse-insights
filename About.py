dda467aae4aae1e04bd5d9eafc79519dc05bdd05
Edima <edimaudo@gmail.com>
2025-08-13 16:57:49 +0000

    update

--- About.py
+++ About.py
@@ -5,7 +5,7 @@ st.header(ABOUT_HEADER)
 
 st.markdown(
     """
-    Focusing on the theme - *Nature's Fury*, this project provides the opporunity to analyze annual greenhouse gas air emissions by Industry and Region. 
+    Focusing on the environmental theme - *Nature's Fury*, this project provides the opporunity to analyze annual global greenhouse gas air emissions by Industry and Region. 
     It is built using [streamlit](https://streamlit.io/cloud) and [Google Gemini](https://aistudio.google.com/).  
     The data is from [Annual Greenhouse Gas (GHG) Air Emissions Accounts](https://climatedata.imf.org/datasets/c8579761f19740dfbe4418b205654ddf/explore?showTable=true).
     """
