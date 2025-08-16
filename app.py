56dafe4164c763d813e8264ab06539c6a8f35845
Edima <edimaudo@gmail.com>
2025-08-16 17:27:48 +0000

    renamed file

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-pg = st.navigation([
-    st.Page("About.py"),
-    st.Page("Planet_Heater.py"),
-    st.Page("Carbon_Chronicles.py"),
-    st.Page("Greener_Faster.py"),
-])
-pg.run()
